ad4351e1e09f547461e95ede05ab144c99723a99
Miguel Pereira <mperenty@gmail.com>
2019-05-14 04:41:28 +0200

    Added a Main class to integrate and run our program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/* This will be the main driver class to execute
+and integrate all the program functionalities
+*/
+
+public class Main {
+
+    public static void main(String[]args)
+    {
+        Graph graph = new Graph();
+        graph.create();
+
+    }
+}
